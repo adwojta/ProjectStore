a705e50d7f7bdc0909fe057b1e12f9b418dadeee
adwojta <illusive.man32@gmail.com>
2021-04-13 16:36:57 +0200

    Setting Spring project

---
+++ SpringFiles/src/test/java/io2/puertolego/PuertolegoApplicationTests.java
@@ -0,0 +1,13 @@
+package io2.puertolego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuertolegoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
