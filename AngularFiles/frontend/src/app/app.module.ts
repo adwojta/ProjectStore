840dca3990457825b3d8a0252fd2973b86d42bfa
kubackiT <80779935+kubackiT@users.noreply.github.com>
2021-04-14 21:24:14 +0200

    creating basic controllers

--- AngularFiles/frontend/src/app/app.module.ts
+++ AngularFiles/frontend/src/app/app.module.ts
@@ -3,10 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProfileComponent } from './profile/profile.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ProductComponent } from './product/product.component';
+import { BasketComponent } from './basket/basket.component';
+import { PromoComponent } from './promo/promo.component';
+import { SeriesComponent } from './series/series.component';
+import { AnnouncementsComponent } from './announcements/announcements.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProfileComponent,
+    CheckoutComponent,
+    ProductComponent,
+    BasketComponent,
+    PromoComponent,
+    SeriesComponent,
+    AnnouncementsComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
