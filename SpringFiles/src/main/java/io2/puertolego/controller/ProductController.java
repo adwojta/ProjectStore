b224dec6a7364b7133c5a66f8f70bccedf3fc07e
kapawelek <karol.pawelekk@gmail.com>
2021-04-15 17:15:17 +0200

    Revert "next push"
    This reverts commit 29be94dbb9f55d788c237c39a18cdabbf15c2209.

--- SpringFiles/src/main/java/io2/puertolego/controller/ProductController.java
+++ SpringFiles/src/main/java/io2/puertolego/controller/ProductController.java
@@ -31,4 +31,8 @@ public class ProductController {
 		
 		return queryResults;
 	}
+	
+	
+	
+	
 }
--- SpringFiles/src/main/java/io2/puertolego/repository/CommentRepo.java
+++ SpringFiles/src/main/java/io2/puertolego/repository/CommentRepo.java
@@ -1,10 +1,5 @@
 package io2.puertolego.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+public class CommentRepo {
 
-import io2.puertolego.models.Comment;
-
-@Repository
-public interface CommentRepo extends JpaRepository<Comment, Integer> {
 }
