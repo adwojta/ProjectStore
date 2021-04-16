24e1fb0428f3be10448f148d7502397ac5a535f4
adwojta <illusive.man32@gmail.com>
2021-04-16 02:00:07 +0200

    Setting config for swagger. Adding swagger in product endpoint

--- SpringFiles/src/main/java/io2/puertolego/PuertolegoApplication.java
+++ SpringFiles/src/main/java/io2/puertolego/PuertolegoApplication.java
@@ -4,10 +4,13 @@ package io2.puertolego;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 
 
 @SpringBootApplication
+@EnableSwagger2
 public class PuertolegoApplication {
 
 	public static void main(String[] args) {
--- SpringFiles/src/main/java/io2/puertolego/controller/ProductController.java
+++ SpringFiles/src/main/java/io2/puertolego/controller/ProductController.java
@@ -7,11 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import io2.puertolego.models.Product;
 import io2.puertolego.repository.ProductRepo;
 
 
 @RestController
+@Api(tags = "Product", description = "Everything about Product")
 public class ProductController {
 	
 	private ProductRepo repo; 
@@ -21,12 +24,14 @@ public class ProductController {
 		this.repo = repo;
 	}
 
+	@ApiOperation(value = "Get all product")
 	@GetMapping("/product/all/")
 	public List<Product> getAllProduct(){
 		List<Product> queryResults = repo.findAll();	
 		return queryResults;
 	}
 	
+	@ApiOperation(value = "Get product by Id")
 	@GetMapping("/product/{id_pro}")
 	public Product getById(@PathVariable int id_pro){
 		//Optional<Product> queryResults = repo.findById(id_pro);
@@ -38,18 +43,21 @@ public class ProductController {
 		return queryResults;
 	}
 	
+	@ApiOperation(value = "Get all collections")
 	@GetMapping("/product/collections/")
 	public List<String> getAllCollection(){
 		List<String> queryResults = repo.getAllCollection();
 		return queryResults;
 	}
 	
+	@ApiOperation(value = "Get collection by Id")
 	@GetMapping("/product/collection/{collection}")
 	public List<Product> getCollectionProducts(@PathVariable String collection ){
 		List<Product> queryResults = repo.getCollectionProducts(collection);
 		return queryResults;
 	}
 	
+	@ApiOperation(value = "Get all promoted product")
 	@GetMapping("/product/promoted/")
 	public List<Product> getPromoted(){
 		List<Product> queryResults = repo.getPromoted();
