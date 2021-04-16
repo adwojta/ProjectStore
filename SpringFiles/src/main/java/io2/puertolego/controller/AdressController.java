ba703efbaaeabf177be9d600a92a1788634d53f4
adwojta <illusive.man32@gmail.com>
2021-04-16 19:38:37 +0200

    Creating Adress endpoint
    Creating Adress endpoint with basic operation - getting client adress, setting client adress and update client adress

---
+++ SpringFiles/src/main/java/io2/puertolego/controller/AdressController.java
@@ -0,0 +1,77 @@
+package io2.puertolego.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io2.puertolego.models.Adress;
+import io2.puertolego.repository.AdressRepo;
+
+
+@RestController
+@Api(tags = "Adress", description = "Everything about Adress")
+public class AdressController {
+	
+	private AdressRepo repo;
+	
+	@Autowired
+	public AdressController(AdressRepo repo) {
+		this.repo = repo;
+	}
+
+	@ApiOperation(value = "Get Client adress")
+	@GetMapping("/adress/{id_client}")
+	public ResponseEntity<?> getAllProduct(@PathVariable(required=true) int id_client, @RequestParam(required=true) String authKey){
+		boolean authorized = repo.requestAuthorization(id_client, authKey);
+		Adress queryResults;
+		
+		if(authorized) {
+			queryResults = repo.getClientAdress(id_client);
+			return new ResponseEntity<Adress>(queryResults,HttpStatus.OK);
+		}
+		else {
+			return new ResponseEntity<String>(HttpStatus.UNAUTHORIZED.toString(), HttpStatus.UNAUTHORIZED);
+		}
+	}
+	
+	@ApiOperation(value = "Adding Client adress")
+	@PostMapping("/adress")
+	public ResponseEntity<?> addNewItem(@RequestParam(required=true) int id_client, @RequestParam(required=true) String authKey, @RequestParam(required=true) String city, @RequestParam(required=true) String country, @RequestParam(required=true) String street, @RequestParam(required=true) String postcode) {
+		boolean authorized = repo.requestAuthorization(id_client, authKey);
+		
+		if(authorized) {
+			repo.addClientAdress(city, country, street, postcode);
+			int id_adr = repo.getNewestRow();
+			repo.addIdAdressToClient(id_adr, id_client);
+			return new ResponseEntity<>(HttpStatus.OK.toString(),HttpStatus.OK);
+		}
+		else {
+			return new ResponseEntity<String>(HttpStatus.UNAUTHORIZED.toString(), HttpStatus.UNAUTHORIZED);
+		}
+
+	}
+	
+	@ApiOperation(value = "Update Client adress")
+	@PutMapping("/adress")
+	public ResponseEntity<?> updateAdress(@RequestParam(required=true) int id_client, @RequestParam(required=true) String authKey,@RequestParam(required=true) int id_adr, @RequestParam(required=true) String city, @RequestParam(required=true) String country, @RequestParam(required=true) String street, @RequestParam(required=true) String postcode) {
+		boolean authorized = repo.requestAuthorization(id_client, authKey);
+		
+		if(authorized) {
+			repo.updateAdress(postcode, city, country, street, id_adr);
+			return new ResponseEntity<>(HttpStatus.OK.toString(),HttpStatus.OK);
+		}
+		else {
+			return new ResponseEntity<String>(HttpStatus.UNAUTHORIZED.toString(), HttpStatus.UNAUTHORIZED);
+		}
+
+	}
+}
--- SpringFiles/src/main/java/io2/puertolego/repository/AdressRepo.java
+++ SpringFiles/src/main/java/io2/puertolego/repository/AdressRepo.java
@@ -1,11 +1,38 @@
 package io2.puertolego.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import io2.puertolego.models.Adress;
 
 @Repository
 public interface AdressRepo extends JpaRepository<Adress, Integer>{
 	
+	@Query(value="select (case when exists (select id_client, authKey from dbo.Client where id_client = ?1 and authKey = ?2 )then 'true' else 'false' end) as authorized",nativeQuery=true)
+	boolean requestAuthorization(int id_client,String authKey);
+	
+	
+	@Query(value="SELECT a.id_adr, a.city, a.country, a.street, a.postcode FROM dbo.Adress a join dbo.Client b on a.id_adr = b.id_adr where b.id_client = ?1",nativeQuery=true)
+	Adress getClientAdress(int id_client);
+	
+	@Query(value="select top 1 id_adr from dbo.Adress order by id_adr desc",nativeQuery=true)
+	int getNewestRow();
+	
+	@Transactional
+	@Modifying
+	@Query(value="insert into dbo.Adress(city,country,street,postcode) values (?1,?2,?3,?4)",nativeQuery=true)
+	void addClientAdress(String city,String country,String street,String postcode);
+	
+	@Transactional
+	@Modifying
+	@Query(value="Update Client set id_adr = ?1 where id_client = ?2",nativeQuery=true)
+	void addIdAdressToClient(int id_adr,int id_client);
+	
+	@Transactional
+	@Modifying
+	@Query(value="Update dbo.Adress set city = ?1, country = ?2, street = ?3, postcode = ?4 where id_adr = ?5",nativeQuery=true)
+	void updateAdress(String city,String country,String street,String postcode,int id_adr);
 }
