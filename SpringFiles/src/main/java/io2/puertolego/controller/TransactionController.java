03e94bf848e148be7b791ca37e48dfcb421c7dc3
Adrian Wojtasik <80778644+adwojta@users.noreply.github.com>
2021-05-25 14:26:21 +0200

    Changed variable name to match with java convection

--- SpringFiles/src/main/java/io2/puertolego/controller/TransactionController.java
+++ SpringFiles/src/main/java/io2/puertolego/controller/TransactionController.java
@@ -1,5 +1,7 @@
 package io2.puertolego.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io2.puertolego.models.Transaction;
 import io2.puertolego.repository.TransactionRepo;
 
 @CrossOrigin(origins = "http://localhost:4200")
@@ -23,12 +26,20 @@ private TransactionRepo repo;
 	@ApiOperation(value = "Get transaction info")
 	@GetMapping("/Transaction/{id_client}")
 	public ResponseEntity<?> getTransactions(@PathVariable int id_client, @RequestParam(required=true) String authKey){
-	return null;
+		boolean authorized = repo.requestAuthorization(id_client, authKey); 
+		
+		if(authorized) {
+			List<Transaction> queryResults = repo.getTransactions(id_client);
+			return new ResponseEntity<List<Transaction>>(queryResults,HttpStatus.OK);
+		}
+		else {
+			return new ResponseEntity<String>(HttpStatus.UNAUTHORIZED.toString(), HttpStatus.UNAUTHORIZED);
+		}
 	}
 
 	@ApiOperation(value = "Add new transaction")
 	@PostMapping("/Transaction/{id_client}")
-	public ResponseEntity<?> addTransaction(@PathVariable int id_client, @RequestParam(required=true) String authKey){
+	public ResponseEntity<?> addTransaction(@PathVariable int id_client, @RequestParam(required=true) String authKey, @RequestParam(required=true) int products){
 	return null;
 	}
 }
